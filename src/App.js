9521be40bdd8579f08ba866182c035ceea364d35
SOFTECH\aftab.ahmed <aftab.ahmed@softech.com.pk>
2021-07-05 21:09:20 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [again,setAgain] = useState(0)
+  const [half,setHalf] = useState(0)
+  const [third,setThird] = useState(0)
+  let myInterval
+
+  const changeColor = () => {
+    if (seconds <= third) {
+    document.getElementById("divChange").style.background = 'red';
+    }
+    else if (seconds <= half) {
+      document.getElementById("divChange").style.background = "yellow";
+    }
+    else if(seconds == 0){
+      document.getElementById("divChange").style.background = 'black';
+    }
+    else if(seconds > half){
+    document.getElementById("divChange").style.background = 'green';
+    }
+  }
+
+  useEffect(() => {
+    if (seconds > 0) {
+      setHalf(parseInt(again/2))
+      setThird(parseInt(again/3))
+      myInterval = setTimeout(() => setSeconds(seconds - 1), 1000);
+      changeColor();
+    } else {
+      setSeconds(again);
+    }
+    return ()=> {
+      clearTimeout(myInterval);
+    };
+  });
+
+  const Start = (e) => {
+    e.preventDefault()
+    setSeconds(e.target.count.value)
+    setAgain(e.target.count.value)
+  }
+
+  const Stop = () => {
+    setSeconds(0)
+    setAgain('')
+    clearTimeout(myInterval);
+    document.getElementById("divChange").style.background = 'black';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id={'divChange'} style={{
+        textAlign:'center',
+        width:'300px',
+        height:'100px',
+        backgroundColor:'grey',
+        margin: '0 auto',
+        padding: '10px',
+        position: 'relative'
+        }}>
+        <h1 style={{color: 'white'}}>{seconds}</h1>
+      </div>
+      <div>
+        <form onSubmit={Start}> 
+        <label for="fname">Enter Value In Seconds:</label>
+        <br/>
+            <input type='text' name='count' placeholder="Enter Value in seconds..." required/>
+            <button type="submit">Start</button>
+         </form>
+         <button onClick={Stop}>Stop</button>
+      </div>
     </div>
-  );
+  )
+  
 }
-
 export default App;
